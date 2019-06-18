c789c5fcdac35a5d9811bb3a1301aaa98edfd823
Dave <43281665+chaos511@users.noreply.github.com>
2019-06-18 09:46:40 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# node-red-contrib-insteon-lan
+
+Nodes to control x10 modules from the insteon hub 
+you need to enter the hubs ip,port,username,password into the nodes config page
+
+i will add insteon lamplinc modules also
+
+## Example
+
+```json
+
+[{"id":"242d432.d2174bc","type":"X10-control","z":"f8c97fa4.80721","name":"","hubip":"192.168.1.24","hubport":"25105","hubuser":"user","hubpass":"pass","x":610,"y":380,"wires":[["c056af8.5bfbf5","966c9ab2.7f0518"]]},{"id":"cc6ad5d0.da8498","type":"inject","z":"f8c97fa4.80721","name":"","topic":"","payload":"{\"housecode\":\"b\",\"unitcode\":\"9\",\"command\":\"OFF\"}","payloadType":"json","repeat":"","crontab":"","once":false,"onceDelay":0.1,"x":410,"y":380,"wires":[["242d432.d2174bc","6c142a3d.cf64f4"]]}]
+
+```
---
+++ x10/x10.js
@@ -0,0 +1,134 @@
+module.exports = function(RED) {
+    function X10controlNode(config) {
+        RED.nodes.createNode(this,config);
+	var port=config.hubport
+	var ip=config.hubip
+	var user=config.hubuser
+	var pass=config.hubpass
+//        const Httprequest1 = new XMLHttpRequest();
+// 	const Httprequest2 = new XMLHttpRequest();
+
+
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var btoa = require('btoa'); 
+var httpr = new XMLHttpRequest()
+var httpr2 = new XMLHttpRequest()
+
+	var housecodelookup=['6','E','2','A','1','9','5','D','7','F','3','B','0','8','4','C']
+        var node = this;
+        node.on('input', function(msg) {
+		node.status({fill:"red",shape:"ring",text:"Input error"})
+//		var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+//		var xhttp = new XMLHttpRequest();
+
+		if(msg.payload.unitcode==null){
+			node.error("unitcode is null/undefined")
+			return
+		}
+		if(msg.payload.housecode==null){
+			node.error("housecode is null/undefined")
+			return
+		}
+		if(msg.payload.housecode.length!=1){
+			node.error("housecode length != 1")
+			return
+		}
+		if(msg.payload.unitcode.length>2){
+			node.error("unitcode >2")
+			return
+		}
+		var housecodenum=msg.payload.housecode.toUpperCase().charCodeAt(0)-65
+		if(housecodenum<0 || housecodenum>15){
+			node.error("housecode not A-P")
+			return
+		}
+		if(msg.payload.unitcode<1 ||msg.payload.unitcode>16){
+			node.error("unitcode not 1-16")
+			return
+		}
+		switch(msg.payload.command.toUpperCase()){
+			case "ON":
+				commandval="280"
+			break;
+			case "OFF":
+				commandval="380"
+			break;
+			case "DIM":
+				commandval="480"
+			break;
+			case "BRIGHT":
+				commandval="580"
+			break;
+			default:
+				node.error("command not ON,OFF,DIM,BRIGHT")
+				break
+			break;
+		}
+
+		var unitcodeval=housecodelookup[msg.payload.unitcode-1]
+		var housecodeval=housecodelookup[housecodenum]
+		var url1="http:\/\/"+user+":"+pass+"@"+ip+":"+port+"/3?0263"+housecodeval+""+unitcodeval+"00=I=3"
+		var url2="http:\/\/"+user+":"+pass+"@"+ip+":"+port+"/3?0263"+housecodeval+""+commandval+"=I=3"
+		node.status({fill:"yellow",shape:"ring",text:"Connecting"})
+		var done=0
+		msg.debug={"url1":url1,"url2":url2}
+		httpr.onreadystatechange = function(){
+			if(this.status==200){
+				if(done==0){
+					setTimeout(function(){
+						httpr2.open("GET",url2)
+						httpr2.setRequestHeader("Authorization", "Basic " + btoa(user+":"+pass));
+						httpr2.send()
+						done=1
+					},1000)
+				}
+			}
+			if(this.status==401){
+				done=401
+				node.send({payload:"Unauthorized"})
+				node.status({fill:"red",shape:"ring",text:"Error Unauthorized "})
+			}
+		}
+
+
+		httpr2.onreadystatechange = function(){
+			if(this.status==200){
+				if(done==1){
+					done=2
+					node.status({fill:"green",shape:"ring",text:"Complete"})
+					node.send(msg)
+				}
+			}
+			if(this.status==401){
+				done=401
+				node.send({payload:"Unauthorized"})
+				node.status({fill:"red",shape:"ring",text:"Error Unauthorized "})
+			}
+		}
+
+		httpr.open("GET",url1)
+		httpr.setRequestHeader("Authorization", "Basic " + btoa(user+":"+pass))
+		httpr.send()
+
+		setTimeout(function(){
+			if(done==0||done==1){
+    				node.status({fill:"red", shape:"ring", text:"Error connecting to hub"});
+				done=-1
+				node.send({payload:"timeout"})
+			}
+//			node.warn("timeout")
+		}, 3000);
+
+
+//		while(!done){}
+//		msg.url=url1
+//		node.send(msg)
+//		node.warn("done")
+//		msg.url=url2
+		//msg.url2=url2
+//                node.send(msg);
+        });
+    }
+
+    RED.nodes.registerType("X10-control",X10controlNode);
+}
